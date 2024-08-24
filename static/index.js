e5c2ecbbff55603481db4f51b15dc6f3389a0535
MonsieurMeta <monsieurmetaverse@outlook.com>
2024-08-24 16:51:55 -0700

    omg

--- static/index.js
+++ static/index.js
@@ -1,35 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savedUsername = localStorage.getItem('username');
-    
-    if (savedUsername) {
+    const savedPassword = localStorage.getItem('password');
+
+    if (savedUsername && savedPassword) {
         window.location.href = 'main.html';
     }
 });
 
-function handleEnter(event) {
-    event.preventDefault();
 
-    const username = document.getElementById('username').value.trim();
-    localStorage.setItem('username', username);
-    localStorage.setItem('coinAmount', 0);
-    localStorage.setItem('metersToday', 0);  // Initialize metersToday with 0
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+localStorage.setItem('coinAmount', 0);
+localStorage.setItem('metersToday', 0);  // Initialize metersToday with 0
 
-    showLoading();
-}
+showLoading();
 
 function showLoading() {
     const button = document.querySelector("#login-form button");
 
     button.innerHTML = '<div class="loader-container"><div class="loader"></div></div>';
-    button.disabled = true; 
+    button.disabled = true;
 
     setTimeout(() => {
-        window.location.href = 'main.html'; 
-    }, 2000); 
+        window.location.href = 'main.html';
+    }, 2000);
 }
 
 function playClickSound() {
-    const isSoundOn = localStorage.getItem('soundToggle') === 'on'; 
+    const isSoundOn = localStorage.getItem('soundToggle') === 'on';
     if (isSoundOn) {
         const clickSound = new Audio('click.mp3');
         clickSound.play();
@@ -47,9 +45,9 @@ function attachClickSound() {
 }
 
 function handleSoundToggleChange() {
-    const soundToggle = document.getElementById('sound-toggle'); 
+    const soundToggle = document.getElementById('sound-toggle');
     soundToggle.addEventListener('change', () => {
-        const isOn = soundToggle.checked; 
+        const isOn = soundToggle.checked;
         localStorage.setItem('soundToggle', isOn ? 'on' : 'off');
     });
 }
